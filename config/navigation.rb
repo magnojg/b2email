a3f5c6dce8d763fde45747b3fbc4bbfcd059d8cb
magnogouveia <magnojg@gmail.com>
2016-04-22 23:56:59 -0300

    a

--- config/navigation.rb
+++ config/navigation.rb
@@ -10,13 +10,5 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :root_menu, raw('<i class="icon-home icon-white"></i>'), companies_path
     primary.item :campaigns_menu, 'Campaigns', campaigns_path
     primary.item :ad_bars_menu, 'Ad Bars', ad_bars_path
-    primary.item :ads_menu, 'Ads', ads_path
-
-    #Workflow
-    # primary.item :workflow_menu, "Workflow", nil do |sub_nav|
-    #   sub_nav.item :workflow_schedules, "Agendamentos", workflow_schedules_path, :highlights_on => :subpath
-    #   sub_nav.item :workflow_holidays, "Feriados", workflow_holidays_path, :highlights_on => :subpath
-    #   sub_nav.item :workflow_calendar_path, "Calendário", workflow_calendar_path, :highlights_on => :subpath
-    # end
   end
 end
