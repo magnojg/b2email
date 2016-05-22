66f459c60c5abdd8b36b013e9cdb23d830d11040
magnogouveia <magnojg@gmail.com>
2016-05-22 19:59:21 -0300

    a

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -11,14 +11,38 @@ $(document).ready(function() {
         tk: 'B2EMAIL'
       },
       type: 'GET',
+      beforeSend: function (jqXHR, settings) {
+        url = location.protocol + "//" + location.host + settings.url;
+        jquery_result = url; //jquery_snippet(url);
+      },
       error: function() {
         $('#bar_result').html('<p>An error has occurred</p>');
       },
       success: function(data) {
-        console.log(data)
         position = data['position']
+        $('#url_call').html(jquery_result);
         $('#' + position).html(data['content']);
       }
     });
   });
 });
+
+function jquery_snippet(url) {
+  var snippet =
+  "$(document).ready(function() {" +
+  "  $.ajax({" +
+  "    url: '/api/v1/ad_bars', " +
+  "    data: { ad_bar_id: ID, tk: 'TOKEN' }, " +
+  "    type: 'GET', " +
+  "    error: function() { " +
+  "      $('#bar_result').html('<p>An error has occurred</p>'); " +
+  "    }, " +
+  "    success: function(data) { " +
+  "      position = data['position']; " +
+  "      $('#' + position).html(data['content']); " +
+  "    } " +
+  "  })" +
+  "})";
+
+  return snippet;
+}
