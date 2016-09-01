4236fbe6d4cc58c6efae6036bdaf31c1b9fedf6f
magnogouveia <magnojg@gmail.com>
2016-08-31 21:48:56 -0300

    a

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,14 +3,14 @@ class Company < ActiveRecord::Base
 
   has_attached_file :logo_image,
 										:default_url => "null",
-										:path => ":id.:style.:filename",
+										:path => "/:company_folder/:id.:style.:filename",
                     :url => ":s3_domain_url"
 
   validates_attachment :logo_image,
 												content_type: { content_type: /\Aimage\/.*\Z/ },
 												size: { in: 0..1.megabytes }
 
-  # Paperclip.interpolates :company_folder do |attachment, style|
-  #   "COMP#{attachment.instance.company.id}"
-  # end
+  Paperclip.interpolates :company_folder do |attachment, style|
+    "COMP#{attachment.instance.id}"
+  end
 end
